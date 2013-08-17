3a8f65aa35a0d4702889f418935097c714e7ca32
Hack Reactor Students <github@hackreactor.com>
2013-08-16 22:30:37 -0700

    convert to functional instantiation and make enemies move every second

--- src/collider.js
+++ src/collider.js
@@ -1,55 +1,75 @@
+var makeGame = function() {
+  var game = {};
 
+  game.gameOptions = {
+    height: 450,
+    width: 700,
+    numberOfEnemies: 30,
+    padding: 20
+  };
 
+  game.stats = {
+    score: 0,
+    bestScore: 0
+  };
 
-var gameOptions = {
-  height: 450,
-  width: 700,
-  numberOfEnemies: 30,
-  padding: 20
-};
+  game.axes = {
+    x: d3.scale.linear().domain([0, 100]).range([0, game.gameOptions.width]),
+    y: d3.scale.linear().domain([0, 100]).range([0, game.gameOptions.height])
+  };
 
-var gameStats = {
-  score: 0,
-  bestScore: 0
-};
+  game.board = d3.select('.board').append('svg:svg')
+  .attr('width', game.gameOptions.width)
+  .attr('height', game.gameOptions.height);
 
-axes = {
-  x: d3.scale.linear().domain([0, 100]).range([0, gameOptions.width]),
-  y: d3.scale.linear().domain([0, 100]).range([0, gameOptions.height])
-};
+  game.createEnemies = function(){
+    var enemiesArray = [];
+    for(var i = 0; i < game.gameOptions.numberOfEnemies; i++){
+      enemiesArray[i] = {
+        id: i,
+        x: Math.random() * 100,
+        y: Math.random() * 100
+      };
+    }
+    return enemiesArray;
+  };
 
-var gameBoard = d3.select('.board').append('svg:svg')
-.attr('width', gameOptions.width)
-.attr('height', gameOptions.height);
+  game.render = function(enemyData){
+    console.log('hello');
+    var enemies;
+    enemies = game.board.selectAll('circle.enemy').data(enemyData, function(d) {
+      return d.id;
+    });
+    enemies.enter().append('svg:circle').attr('class', 'enemy')
+      .attr('cx', function(enemy){
+        return game.axes.x(enemy.x);
+      })
+      .attr('cy', function(enemy){
+        return game.axes.y(enemy.y);
+      })
+      .attr('r', 0);
+    enemies.exit().remove();
+    enemies.transition().duration(500).attr('r', 10)
+      .attr('cx', function(enemy){
+        return game.axes.x(enemy.x);
+      })
+      .attr('cy', function(enemy){
+        return game.axes.y(enemy.y);
+      });
+  };
 
-var createEnemies = function(){
-  var enemiesArray = [];
-  for(var i = 0; i < gameOptions.numberOfEnemies; i++){
-    enemiesArray[i] = {
-      id: i,
-      x: Math.random() * 100,
-      y: Math.random() * 100
+  game.play = function() {
+    var moveEnemies;
+    moveEnemies = function() {
+      var newEnemyPositions;
+      newEnemyPositions = game.createEnemies();
+      return game.render(newEnemyPositions);
     };
-  }
-  return enemiesArray;
+    moveEnemies();
+    setInterval(moveEnemies, 1000);
+  };
+  return game;
 };
 
-var render = function(enemyData){
-  var enemies;
-  enemies = gameBoard.selectAll('circle.enemy').data(enemyData, function(d) {
-    return d.id;
-  });
-  enemies.enter().append('svg:circle').attr('class', 'enemy')
-    .attr('cx', function(enemy){
-      return axes.x(enemy.x);
-    })
-    .attr('cy', function(enemy){
-      return axes.y(enemy.y);
-    })
-    .attr('r', 0);
-  enemies.exit().remove();
-  //enemies.transition().duration(500).attr('r', 10).transition().duration(1000).tween('custom', createEnemies);
-};
-
-var newEnemies = createEnemies();
-render(newEnemies);
+var newGame = makeGame();
+newGame.play();
